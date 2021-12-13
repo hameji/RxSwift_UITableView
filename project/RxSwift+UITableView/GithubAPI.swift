092e7ea1a60175cf0ba68f26f62d8b8c53e8b08c
Hajime Taniguchi <hameji@gmail.com>
2021-12-12 20:53:46 -0500

    githubAPIを編集

--- project/RxSwift+UITableView/GithubAPI.swift
+++ project/RxSwift+UITableView/GithubAPI.swift
@@ -7,25 +7,44 @@
 
 import Foundation
 
+extension FetchRepositoryError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .noInput:
+            return NSLocalizedString("There was no input for user", comment: "NoInput")
+        case .wrongURL:
+            return NSLocalizedString("The URL did not exist", comment: "WrongURL")
+        case .noData:
+            return NSLocalizedString("There was no data at url", comment: "NoData")
+        case .parse:
+            return NSLocalizedString("Failed to decode json", comment: "Parse")
+        }
+    }
+}
+
+enum FetchRepositoryError: Error {
+    case noInput
+    case wrongURL
+    case noData
+    case parse
+}
+
+// https://api.github.com/users/hameji/repos
+
 class GithubAPI {
     private static var task: URLSessionTask?
     
     private static let usersURL = "https://api.github.com/users/"
     private static let repos = "/repos"
-
-    enum FetchRepositoryError: Error {
-        case noInput
-        case wrongURL
-        case noData
-        case parse
-    }
+    
+    private static let url = "https://api.github.com/search/repositories?q=Swift"
     
     static func fetchRepository(text: String, completionHandler: @escaping (Result<[Repository], Error>) -> Void) {
         guard !text.isEmpty else {
             completionHandler(.failure(FetchRepositoryError.noInput))
             return
         }
-        let urlString = Self.usersURL + text + Self.repos
+        let urlString = Self.url // Self.usersURL + text + Self.repos
         guard let url = URL(string: urlString) else {
             completionHandler(.failure(FetchRepositoryError.wrongURL))
             return
